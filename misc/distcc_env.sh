69d1e41c4663733763ea01b5876e9a2798944753
tjanson <priv.tom.janson@gmail.com>
2014-09-16 17:44:24 +0200

    distcc log in local folder

--- misc/distcc_env.sh
+++ misc/distcc_env.sh
@@ -1,7 +1,7 @@
 export DISTCC_HOSTS=vagrant-ubuntu-trusty-64.lan,cpp,lzo
 export DISTCC_SKIP_LOCAL_RETRY=1
 export DISTCC_VERBOSE=1
-export DISTCC_LOG=/var/log/distcc.log
+export DISTCC_LOG=distcc.log
 export DISTCC_BACKOFF_PERIOD=0
 export DISTCC_FALLBACK=0
 export PATH=/usr/lib/distcc:${PATH}
